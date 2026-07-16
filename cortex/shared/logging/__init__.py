e3802e1ba6d7292e8e0de1ef411a5915e33b81ab
David Pinheiro <dkspinheiro@gmail.com>
2026-07-15 23:20:07 -0300

    feat: configure structured logging

---
+++ cortex/shared/logging/__init__.py
@@ -0,0 +1,7 @@
+from cortex.shared.logging.logger import logger
+from cortex.shared.logging.setup import configure_logging
+
+__all__ = [
+    "logger",
+    "configure_logging",
+]
---
+++ cortex/shared/logging/setup.py
@@ -0,0 +1,20 @@
+import logging
+
+import structlog
+
+
+def configure_logging() -> None:
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.INFO,
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.JSONRenderer(),
+        ],
+        logger_factory=structlog.PrintLoggerFactory(),
+    )
